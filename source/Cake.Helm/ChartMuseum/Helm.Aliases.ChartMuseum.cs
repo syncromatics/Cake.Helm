ab30607bdb80cbddb4bf76cf338bf4602183a650
Daniel James <djames@syncromatics.com>
2018-08-06 10:20:18 -0700

    (+semver: feature) Add HelmChartMuseumUpload alias to upload Helm chart packages to a ChartMuseum

---
+++ source/Cake.Helm/ChartMuseum/Helm.Aliases.ChartMuseum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Cake.Core.Annotations;
+using Cake.Helm.ChartMuseum;
+
+namespace Cake.Helm
+{
+    partial class HelmAliases
+    {
+        [CakeMethodAlias]
+        public static void HelmChartMuseumUpload(this Cake.Core.ICakeContext context, HelmChartMuseumUploadSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            var requestUri = new Uri(new Uri(settings.MuseumUrl), "api/charts");
+            var file = new FileInfo(settings.Path);
+            using (var client = new HttpClient())
+            using (var stream = file.OpenRead())
+            {
+                if (!string.IsNullOrEmpty(settings.Username))
+                {
+                    var byteArray = Encoding.ASCII.GetBytes($"{settings.Username}:{settings.Password}");
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                }
+                var response = client.PostAsync(requestUri, new StreamContent(stream)).Result;
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
---
+++ source/Cake.Helm/ChartMuseum/HelmChartMuseumUploadSettings.cs
@@ -0,0 +1,28 @@
+﻿namespace Cake.Helm.ChartMuseum
+{
+    /// <summary>
+    /// Settings for uploading charts to a chart musuem
+    /// </summary>
+    public sealed class HelmChartMuseumUploadSettings : GlobalHelmSettings
+    {
+        /// <summary>
+        /// Root URL of the ChartMuseum Helm chart repository
+        /// </summary>
+        public string MuseumUrl { get; set; }
+
+        /// <summary>
+        /// Path to packaged chart to upload to ChartMuseum
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Optional username to authenticate the request to the ChartMusuem (using HTTP basic authentication)
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Optional password to authenticate the request to the ChartMusuem (using HTTP basic authentication)
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
